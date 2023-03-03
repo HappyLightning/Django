7f33520977324ba5adc6afd38a871916177b63f7
HappyLightning <81601280+HappyLightning@users.noreply.github.com>
2023-03-03 16:40:06 -0300

    Delete reviews directory

--- reviews/game/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Jogo
-
-
-# Create your views here.
-def homepage(request):
-    jogos = Jogo.objects.all()
-    return render(request, 'games/home.html', {'jogos': jogos})
